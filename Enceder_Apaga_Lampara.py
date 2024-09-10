b620645bfb5962764092de583a3dd38a2b1d0ef9
Nefer Alvarez <neferalvarez@gmail.com>
2024-09-09 20:10:20 -0500

    Version V1.0

--- Enceder_Apaga_Lampara.py
+++ Enceder_Apaga_Lampara.py
@@ -43,7 +43,7 @@ def main():
   x > Salir'''
     while True:
         print(menu)
-        op = input("Que opcion desea: ")
+        op = input("Por Favor Que opcion desea : ")
         if op == '0':
             lamp.apagar()
         elif op == '1':
